7f1811221fa0695f0ab421743e5fa7655d26389c
Kyle Thousand <kylethousand@kyles-mbp-2.lan>
2020-11-16 17:08:51 -0500

    kyle thousand

--- longest_word.rb
+++ longest_word.rb
@@ -1,7 +1,8 @@
 # RUBY LONGEST WORD
 
 def longest_word(str)
-
+  longest = str.split(" ").sort_by!(&:length) # split the string into an array of words, sort by length, the last element will be the longest.
+  longest[-1]
 end
 
 puts longest_word("The quick brown fox jumped over the lazy dog")
